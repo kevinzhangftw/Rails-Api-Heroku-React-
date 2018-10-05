8a7072f3d27263b47c982a35c3891b2d37c1d812
Kai Kevin <kevin.c.t.zhang@gmail.com>
2018-10-04 21:15:19 -0700

    update api controllers

---
+++ app/controllers/drinks_controller.rb
@@ -0,0 +1,15 @@
+class DrinksController < ApiController
+  before_action :set_drink, only: [:show, :update, :destroy]
+
+  # GET /drinks
+  def index
+    @drinks = Drink.select("id, title").all
+    render json: @drinks.to_json
+  end
+
+  # GET /drinks/:id
+  def show
+    @drink = Drink.find(params[:id])
+    render json: @drink.to_json(:include => { :ingredients => { :only => [:id, :description] }})
+  end
+end
